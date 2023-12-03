1350aa468aa6aa15c3ca71dd7ef22108a0b7a38d
sophxmia <maliarenko.sofiia@gmail.com>
2023-12-03 10:02:40 +0200

    Started working on dark theme

--- script/app.js
+++ script/app.js
@@ -4,3 +4,8 @@ const navLink = document.querySelector(".nav_link");
 hamburger.addEventListener("click", () => {
   navLink.classList.toggle("hide");
 });
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
